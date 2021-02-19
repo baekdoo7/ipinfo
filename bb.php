3f549fcea16bf5285975c89367f577654859fc6a
baekdoo71@gmail.com <qoren71@>
2021-02-19 17:14:21 +0900

    함수테스

--- bb.php
+++ bb.php
@@ -1,6 +1,28 @@
 <?php
 
-phpinfo();
-1
+
 ?>
 
+테스트
+
+<script>
+    login1 = function (){
+        console.log('test');
+         var test = "문어";
+    };
+    login = function(){
+        console.log(login1.test);
+        return {
+            bbb:function (){
+                console.log('test1234');
+            },
+            ccc:function(msg){
+                console.log(msg);
+            },
+            ddd:"오징어"
+        }
+    }();
+</script>
+
+<button onclick="login1('오징어')">눌러</button>
+<button onclick="login.ccc('오징어문어');">눌러2</button>
